13d33d6c6469bd535287facd25ab72e9e7d31e4f
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-10-15 17:36:36 +0100

    Update SMASS_plots.R

--- SMASS_plots.R
+++ SMASS_plots.R
@@ -231,9 +231,6 @@ scot = map_data('world') %>%
   # select(region) %>% 
   # distinct() %>% View()
   filter(region == 'UK') 
-# %>% 
-#   # select(subregion) %>% 
-#   # distinct()
 #   filter(subregion == 'Scotland')
 
 ggplot(data = scot) +
@@ -245,8 +242,94 @@ ggplot(data = scot) +
            col = 'black',
            size = 0.5,
            fill = 'white')+
+  coord_fixed(ratio = 1.3, 
+              xlim = c(-8,
+                       0), 
+              ylim = c(55, 
+                       59))+
   theme(panel.grid = element_blank(), 
-        axis.title = element_blank(), 
-        axis.text = element_blank())
+        axis.title = element_blank()
+        # axis.text = element_blank(),
+        )
+
+
+# Plot1 - Year by year variation ---------------------------------------------------------
+
+plot1_pal = c('#184e77', 
+              '#1e6091', 
+              '#1a759f',
+              '#168aad',
+              '#34a0a4',
+              '#52b69a',
+              '#76c893',
+              '#99d98c', 
+              '#b5e48c', 
+              '#d9ed92', 
+              '#d81159', 
+              '#f72585')
+
+
+
+smass$subclass = factor(smass$subclass, 
+                        levels = c('Harbour porpoise', 
+                                   'Pelagic delphinid', 
+                                   'Mysticete', 
+                                   'Sperm/beaked whale', 
+                                   'Bottlenose dolphin', 
+                                   'Cetacean (indeterminate species)', 
+                                   'Grey seal', 
+                                   'Harbour seal', 
+                                   'Pinniped (Other)', 
+                                   'Pinniped (indeterminate species)', 
+                                   'Marine turtle', 
+                                   'Shark'))
+
+smass$class_field = factor(smass$class_field, 
+                           levels = c('Cetacean', 
+                                      'Pinniped', 
+                                      'Marine Turtle', 
+                                      'Shark'))
+
+ smass_year = smass %>% 
+  separate(col = datefound, 
+           into = c('day', 
+                    'month',
+                    'year'), 
+           sep = '/') %>% 
+  # filter(year == 2022) %>%
+   filter(year %in% c(1992, 
+                      2022)) %>% 
+  group_by(class_field, 
+           subclass) %>% 
+  # na.omit() %>% 
+  ggplot(aes(x = subclass))+
+  geom_bar(col = 'black', 
+           aes(fill = subclass))+
+  scale_fill_manual(values = plot1_pal)+
+  labs(y = 'Number of strandings')+
+  facet_grid(~class_field, 
+             scales = 'free')+
+  facet_grid(~year, 
+             scales = 'free')+
+  theme(panel.grid.major.x = element_blank(), 
+        axis.title.x = element_blank(), 
+        # axis.text.x = element_text(size = 10, 
+        #                            angle = 90),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.y = element_text(size = 14), 
+        axis.text.y = element_text(size = 12), 
+        strip.background = element_rect(fill = 'white'), 
+        strip.text = element_text(size = 12, 
+                                  face = 'bold'), 
+        legend.title = element_blank())
+
+
+ggsave('Smass_plot1.tiff', 
+       plot = smass_plot1,
+       dpi = 'retina', 
+       units = 'cm', 
+       width = 25, 
+       height = 15)
 
 
